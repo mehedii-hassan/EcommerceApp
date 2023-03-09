9ae4a246dea65bdb53cd855edc142bf6fd04ea4e
mehedi_hassan <mehedii.hassan01@gmail.com>
2023-03-09 22:46:10 +0600

    three layout design done

---
+++ app/src/main/java/com/example/ecommerceappdemo/ui/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerceappdemo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecommerceappdemo.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
--- app/src/main/java/com/example/ecommerceappdemo/ui/ShoppingActivity.kt
+++ app/src/main/java/com/example/ecommerceappdemo/ui/ShoppingActivity.kt
@@ -1,11 +1,12 @@
-package com.example.ecommerceappdemo
+package com.example.ecommerceappdemo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecommerceappdemo.R
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
